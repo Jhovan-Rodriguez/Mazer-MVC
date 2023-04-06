deae1c1bdfcb9c65a62761455f1224e8a6ea0fca
Jhovan-Rodriguez <2030295@upv.edu.mx>
2023-04-05 20:07:12 -0600

    Front-End Modulo Login/Dashboard

---
+++ layouts/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2023 &copy; WatchZone</p>
+        </div>
+        <div class="float-end">
+            <p>
+                Creado por
+                <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                <a href="http://watchzone.me">WatchZone</a>
+            </p>
+        </div>
+    </div>
+</footer>
